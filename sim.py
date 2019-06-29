528de63b838d1cb5f97b09345fd0668ace0504a0
Tyler Norbury <tylernorbury96@gmail.com>
2019-06-29 16:57:59 -0700

    Added command line arguments for height and width

--- sim.py
+++ sim.py
@@ -1,7 +1,19 @@
 from world import World
+import argparse
 
 def main():
-    world = World(75, 25)
+
+    # Define the arguments for the program
+    parser = argparse.ArgumentParser(description="Generates a world and simulates life on it")
+    parser.add_argument("--width", dest="width", nargs="?", metavar="width", default=10, type=int, help="Set the width of the world")
+    parser.add_argument("--height", dest="height", nargs="?", metavar="height", default=10, type=int, help="Set the height of the world")
+
+    args = parser.parse_args()
+
+    width = args.width
+    height = args.height
+
+    world = World(width, height)
     world.generateWorld()
     world.displayWorld()
 
